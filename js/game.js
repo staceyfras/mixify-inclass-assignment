c135170279e9d6ec6b9818323e844c795133d38d
Joshua Ferrara <scjosh2@gmail.com>
2019-03-20 21:25:02 -0700

    Styling like 95% done

--- js/game.js
+++ js/game.js
@@ -1,17 +1,34 @@
+//select button for gold
 var upgButton = document.querySelector('button.btn-up-gold');
 var downgButton = document.querySelector('button.btn-down-gold');
 var leftgButton = document.querySelector('button.btn-left-gold');
 var rightgButton = document.querySelector('button.btn-right-gold');
-
+//select buttons for white
 var upwButton = document.querySelector('button.btn-up-white');
 var downwButton = document.querySelector('button.btn-down-white');
 var leftwButton = document.querySelector('button.btn-left-white');
 var rightwButton = document.querySelector('button.btn-right-white');
-
+//if key pressed
 var keys = document.querySelector('html');
-
+var bob = {
+    x = 1;
+    y = 1;
+}
+var alice = {
+    x = 9;
+    y = 9;
+}
+var g = document.getElementsByClassName('player bob');
+    console.log(g[0].style);
+    g[0].style.display = "grid";
+    g[0].style.gridRowStart = bob.x;
+//functions for moving gold
 function MoveUpg(){
     console.log("upg");
+    
+    
+    g[0].style.gridRowStart = bob.x += 1;
+
 }
 function MoveDowng(){
     console.log("downg");
@@ -23,7 +40,7 @@ function MoveRightg(){
     console.log("rightg");
 }
 
-///
+//functions for moving white
 function MoveUpw(){
     console.log("upw");
 }
@@ -36,7 +53,7 @@ function MoveLeftw(){
 function MoveRightw(){
     console.log("rightw");
 }
-///
+//if glod buttons are clicked
 upgButton.onclick = function() {
     MoveUpg();
 }
@@ -49,7 +66,7 @@ leftgButton.onclick = function() {
 rightgButton.onclick = function() {
     MoveRightg();
 }
-///
+//if white buttons are clicked
 upwButton.onclick = function() {
     MoveUpw();
 }
@@ -62,7 +79,7 @@ leftwButton.onclick = function() {
 rightwButton.onclick = function() {
     MoveRightw();
 }
-//keyboard input
+//if key is pressed
 keys.onkeypress = function(){
     var x = event.key;
     switch(x) {
@@ -89,93 +106,6 @@ keys.onkeypress = function(){
             break;
         case 'l':
           MoveRightw();
-            break;
-        default:
-      }
-      
-};
- rightgButton = document.querySelector('button.btn-right-gold');
-
-var upwButton = document.querySelector('button.btn-up-white');
-var downwButton = document.querySelector('button.btn-down-white');
-var leftwButton = document.querySelector('button.btn-left-white');
-var rightwButton = document.querySelector('button.btn-right-white');
-
-function MoveUpg(){
-    console.log("upg");
-}
-function MoveDowng(){
-    console.log("downg");
-}
-function MoveLeftg(){
-    console.log("leftg");
-}
-function MoveRightg(){
-    console.log("rightg");
-}
-function MoveUpw(){
-    console.log("upw");
-}
-function MoveDownw(){
-    console.log("downw");
-}
-function MoveLeftw(){
-    console.log("leftw");
-}
-function MoveRightw(){
-    console.log("rightw");
-}
-
-upgButton.onclick = function() {
-    MoveUpg();
-}
-downgButton.onclick = function() {
-    MoveDowng();
-}
-leftgButton.onclick = function() {
-    MoveLeftg();
-}
-rightgButton.onclick = function() {
-    MoveRightg();
-}
-upwButton.onclick = function() {
-    
-}
-downwButton.onclick = function() {
-  
-}
-leftwButton.onclick = function() {
-  
-}
-rightwButton.onclick = function() {
-  
-}
-.onkeypress = function(){
-    var x = event.key;
-    switch(x) {
-        case 'w':
-            MoveUpg();
-          break;
-        case 'a':
-            MoveLeftg();
-          break;
-        case 's':
-            MoveDowng();
-          break;
-        case 'd':
-            MoveRightg();
-          break;
-        case 'i':
-          MoveUpg();
-            break;
-        case 'j':
-          MoveLeftg();
-            break;
-        case 'k':
-          MoveDowng();
-            break;
-        case 'l':
-          MoveRightg();
             break;
         default:
       }
